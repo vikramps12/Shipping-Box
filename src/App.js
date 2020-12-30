55b6660faaac0875483b829ada0469b871dc1a5f
vikramps12 <ps_vikram12@yahoo.com>
2020-12-30 19:21:07 +0530

    First Comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ShippingForm from './views/ShippingForm';
+import ListShippings from './views/ListShippings'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <NavBar navLink={{ path: "/orders", Lable: "View Orders" }} />
+            <ShippingForm />
+          </Route>
+          <Route path="/orders">
+            <NavBar navLink={{ path: "/", Lable: "Home" }} />
+            <ListShippings />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
